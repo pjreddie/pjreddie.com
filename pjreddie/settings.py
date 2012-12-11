56133abd21bedf194e8f0597392c050f5dd24ee4
Joe Redmon <pjreddie+github@gmail.com>
2012-12-11 15:06:50 -0800

    Not tracking static files

--- pjreddie/settings.py
+++ pjreddie/settings.py
@@ -62,7 +62,8 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = get_path('static')
+STATIC_ROOT = get_path('static/')
+print STATIC_ROOT
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
