c29edf755f95732d343baf57cb1871e94b0301ee
Joe Redmon <pjreddie+github@gmail.com>
2012-12-11 15:15:47 -0800

    Serving static files, but poorly...

--- pjreddie/urls.py
+++ pjreddie/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -15,4 +16,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+	(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.STATIC_ROOT}),
 )
