3975169c84b5e06eccb616aea660aa8a24f5a498
Joe Redmon <pjreddie+github@gmail.com>
2012-12-11 14:15:39 -0800

    Personal Website

---
+++ pjreddie/core/views.py
@@ -0,0 +1,6 @@
+# Create your views here.
+from django.shortcuts import render
+
+def index(request):
+	
+	return render(request, 'index.html')
---
+++ pjreddie/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'pjreddie.views.home', name='home'),
+    # url(r'^pjreddie/', include('pjreddie.foo.urls')),
+
+    url(r'^$', 'pjreddie.core.views.index', name='index'),
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
