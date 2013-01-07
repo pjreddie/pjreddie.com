3a73a531e438b15c3abe84be443fa508efadf75c
Joe Redmon <pjreddie+github@gmail.com>
2013-01-07 07:24:53 -0600

    Update fixtures

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-/manage.py dumpdata core > pjreddie/core/fixtures/initial_data.json
+./manage.py dumpdata core > pjreddie/core/fixtures/initial_data.json
 git commit -a -m "Update fixtures"
 git push heroku master
 heroku run python manage.py migrate
