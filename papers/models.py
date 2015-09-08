cb903d7bbd0dd92fb0b0e8dff3b8b2f234335aac
Joseph Redmon <pjreddie@gmail.com>
2015-09-08 11:21:34 -0700

    Reviews for papers field

--- papers/models.py
+++ papers/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from pjreddie.utils import markdown_to_html
+from django.template.defaultfilters import slugify
 
 class Paper( models.Model ):
     title = models.CharField( max_length=200 )
@@ -9,7 +11,17 @@ class Paper( models.Model ):
     arxiv = models.URLField(max_length=1000, null=True, blank=True)
     link = models.URLField(max_length=1000, null=True, blank=True)
     pdf = models.FileField(upload_to="files/papers", null=True, blank=True)
+    reviews = models.TextField(null=True, blank=True)
+    slug = models.SlugField(blank=True, null=True)
     def __unicode__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Paper, self).save(*args, **kwargs)
+
+    def html(self):
+        return markdown_to_html( self.reviews, [] )
 
 # Create your models here.
--- papers/views.py
+++ papers/views.py
@@ -7,3 +7,8 @@ from django.forms import ModelForm
 def papers(request):
     p = Paper.objects.all().order_by('-date')
     return render(request, 'papers.html', {'papers':p})
+
+def reviews(request, slug):
+    p = get_object_or_404(Paper, slug = slug)
+    return render(request, 'reviews.html', {'paper':p})
+
